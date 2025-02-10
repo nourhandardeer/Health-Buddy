2896046eb61fafdfc7e71d5d5e0baf4c83790cdb
nourhandardeer <nourhandardeer3@gmail.com>
2025-02-11 01:56:42 +0200

    update Medications page

--- lib/NavigationBar/home_page.dart
+++ lib/NavigationBar/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:graduation_project/AddMed/addmed.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -17,34 +17,7 @@ class HomePage extends StatelessWidget {
             fit: BoxFit.cover,
           ),
           const SizedBox(height: 250),
-          Text(
-            "Add your meds to be reminded on time and\ntrack your health",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 18,
-              color: Colors.grey[600],
-            ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => AddMedicationPage()),
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-            ),
-            child: const Text(
-              "Add a med",
-              style: TextStyle(fontSize: 16, color: Colors.blue),
-            ),
-          ),
-          const SizedBox(height: 20),
+          
         ],
       ),
     );
--- lib/NavigationBar/medications_page.dart
+++ lib/NavigationBar/medications_page.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
-
+import 'package:graduation_project/AddMed/addmed.dart';
 
 class MedicationsPage extends StatelessWidget {
   const MedicationsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Image.asset(
+            'images/syringe.png',
+            width: 150,
+            height: 150,
           
-          
-          Text('Medications Page '),
-          
-          
-
+          ),
+          const SizedBox(height: 50),
+          Text(
+            "Add your meds to be reminded on time and\ntrack your health",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.grey[600],
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddMedicationPage()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+            ),
+            child: const Text(
+              "Add a med",
+              style: TextStyle(fontSize: 16, color: Colors.blue),
+            ),
+          ),
+          const SizedBox(height: 20),
         ],
       ),
     );
