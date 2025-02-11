9a48496d2ab74e9c873fc34e3dbfe321b0857969
nourhandardeer <nourhandardeer3@gmail.com>
2025-02-11 13:57:47 +0200

    .

--- lib/NavigationBar/medications_page.dart
+++ lib/NavigationBar/medications_page.dart
@@ -6,16 +6,45 @@ class MedicationsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Image.asset(
+            'images/syringe.png',
+            width: 150,
+            height: 150,
           
-          
-          Text('Medications Page '),
-          
-          
-
+          ),
+          const SizedBox(height: 50),
+          Text(
+            "Add your meds to be reminded on time and\ntrack your health",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 18,
+              color: Colors.grey[600],
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddMedicationPage()),
+              );
+            },
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+            ),
+            child: const Text(
+              "Add a med",
+              style: TextStyle(fontSize: 16, color: Colors.blue),
+            ),
+          ),
+          const SizedBox(height: 20),
         ],
       ),
     );
