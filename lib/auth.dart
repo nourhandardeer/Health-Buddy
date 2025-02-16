829b5da08079ea8163342e2b5795be0caefc8697
elafAhmed <122807606+elafahmed@users.noreply.github.com>
2025-02-16 17:49:01 +0200

    aa

--- lib/auth.dart
+++ lib/auth.dart
@@ -3,9 +3,36 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
   User? get currentUser => _firebaseAuth.currentUser;
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
-  Future <void> signInWithEmailAndPassword({
+
+  Future<String?> createUserWithEmailAndPassword({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Save user data to Firestore
+      await FirebaseFirestore.instance.collection('users').doc(userCredential.user!.uid).set({
+        'firstName': firstName,
+        'lastName': lastName,
+        'email': email,
+      });
+
+      return null; // No error, success
+    } on FirebaseAuthException catch (e) {
+      return e.message; // Return error message
+    }
+  }
+
+  Future<UserCredential> signInWithEmailAndPassword({
     required String email,
     required String password,
   }) async {
@@ -14,16 +41,8 @@ class Auth {
       password: password,
     );
   }
-  Future <void> createUserWithEmailAndPassword({
-    required String email,
-    required String password,
-  }) async {
-    await _firebaseAuth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-  }
-  Future <void> signOut() async {
+
+  Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
 }
